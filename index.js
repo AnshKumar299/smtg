edfb4a5e3d84b20cd40e7705c4b59d16445cf158
Ansh Kumar Singh <singhanshkumar299@gmail.com>
2026-05-24 23:52:40 +0530

    fk

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ app.get('/', (req, res) => {
   return res.json({msg:"'Hello from Server"});
 });
 
-app.listen(PORT, () => {
+app.listen(PORT,"0.0.0.0", () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
