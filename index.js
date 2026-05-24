e294f2b71e9bf502aa9affb8fe46662fdd5e5ab2
Ansh Kumar Singh <singhanshkumar299@gmail.com>
2026-05-25 01:17:48 +0530

    CI/CD working fine

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 8080;
 
 app.get('/', (req, res) => {
-  return res.json({msg:"'Hello from Server vers2"});
+  return res.json({msg:"'Hello from Server vers3. If this works, it means that the CI/CD pipeline is working correctly and the new version of the server has been deployed successfully."});
 });
 
 app.listen(PORT,"0.0.0.0", () => {
